fca57a3a6c36de7245f548089140ebe5648111f7
lucky <takuto.yoshikai@gmail.com>
2021-08-25 19:30:54 +0900

    indent

--- nclock/nclock.py
+++ nclock/nclock.py
@@ -13,8 +13,8 @@ def main():
     root = tkinter.Tk()
     root.title("Nuru Clock")
     width = root.winfo_screenwidth()
-    width = width - 200
-    root.geometry("200x70+" + str(width) + "+0")
+    y = width - 200
+    root.geometry("200x70+" + str(y) + "+0")
 
     jp_label = tkinter.Label(text="00:00")
     id_label = tkinter.Label(text="00:00")
@@ -26,14 +26,15 @@ def main():
         while True:
             jp_now = datetime.datetime.now(tz=pytz.timezone("Asia/Tokyo"))
             id_now = jp_now - datetime.timedelta(hours=2)
+
             jp_time = format(jp_now.hour, "02") + ":" + format(jp_now.minute, "02")
             id_time = format(id_now.hour, "02") + ":" + format(id_now.minute, "02")
+
             jp_label["text"] = jp_time
             id_label["text"] = id_time
             if jp_now.hour == 19 and jp_now.minute == 30 and jp_now.second == 0:
                 playsound(sound_path)
             time.sleep(1)
-
 
     jp_label.pack()
     id_label.pack()
